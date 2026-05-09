d15a32f9442064e5161faa4e8dedd7ab74dbf3ed
Andrew Landry <andrewlandry@gmail.com>
2026-05-09 04:05:42 -0400

    Remove unused Japanese date tool shells

--- client/src/features/tools/japaneseDate/shells/AppJapaneseDateToolShell.tsx
+++
@@ -1,9 +0,0 @@
-import JapaneseDateToolPage from '../components/JapaneseDateToolPage';
-
-const AppJapaneseDateToolShell = () => (
-  <div className="mx-auto w-full max-w-5xl">
-    <JapaneseDateToolPage />
-  </div>
-);
-
-export default AppJapaneseDateToolShell;
